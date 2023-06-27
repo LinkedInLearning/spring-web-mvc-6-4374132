d03e2e2b3a7dabca4179fe2f23cd88ec679526d7
ketkeearyamane <ketkeearyamane@gmail.com>
2023-06-27 12:49:41 +0200

    first commit

---
+++ src/main/java/com/linkedin/collectibles/BigStarCollectiblesApplication.java
@@ -0,0 +1,13 @@
+package com.linkedin.collectibles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigStarCollectiblesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigStarCollectiblesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linkedin/collectibles/BigStarCollectiblesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkedin.collectibles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigStarCollectiblesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
