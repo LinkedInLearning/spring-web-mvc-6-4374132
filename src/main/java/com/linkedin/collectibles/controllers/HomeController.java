1f4c969a88a124bc1dab38ea0ebe1b8fdafe10cb
ketkeearyamane <ketkeearyamane@gmail.com>
2023-06-29 13:39:54 +0200

    link character templates and add UI fragments in character files

--- src/main/java/com/linkedin/collectibles/controllers/HomeController.java
+++ src/main/java/com/linkedin/collectibles/controllers/HomeController.java
@@ -3,6 +3,7 @@ package com.linkedin.collectibles.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
@@ -11,5 +12,10 @@ public class HomeController {
     public String displayHome(Model model){
         return "index";
 
+    }
+
+    @GetMapping("/getCharacter/{charname}")
+    public String getCharacter(@PathVariable("charname") String charName){
+        return "/characters/"+charName;
     }
 }
