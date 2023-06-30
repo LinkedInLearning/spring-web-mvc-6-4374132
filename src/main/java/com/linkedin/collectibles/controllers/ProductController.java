3f5612e08d2b2161f71230fd50aa1a9a3d38a295
ketkeearyamane <ketkeearyamane@gmail.com>
2023-06-30 15:00:34 +0200

    connect UI, controller and data access

--- src/main/java/com/linkedin/collectibles/controllers/ProductController.java
+++ src/main/java/com/linkedin/collectibles/controllers/ProductController.java
@@ -1,13 +1,30 @@
 package com.linkedin.collectibles.controllers;
 
+import com.linkedin.collectibles.beans.Product;
+import com.linkedin.collectibles.dao.ProductRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class ProductController {
 
+    private ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @PostMapping("/search")
-    public String search(){
+    public String search(@RequestParam("searchString") String keyword, Model model){
+
+        List<Product> products = productRepository.searchByName(keyword);
+        model.addAttribute("products", products);
+        model.addAttribute("searchedFor", keyword);
+
         return "search-results";
     }
 }
