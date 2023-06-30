de031e0c64e63c7f33ffed26a3c972df018f7e4b
ketkeearyamane <ketkeearyamane@gmail.com>
2023-06-30 14:51:51 +0200

    add ProductRepository to access DB

---
+++ src/main/java/com/linkedin/collectibles/dao/ProductRepository.java
@@ -0,0 +1,17 @@
+package com.linkedin.collectibles.dao;
+
+import com.linkedin.collectibles.beans.Product;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+
+    @Query("select p from Product p where p.name like %:searchString%")
+    public List<Product> searchByName(@Param("searchString") String keyword);
+
+}
