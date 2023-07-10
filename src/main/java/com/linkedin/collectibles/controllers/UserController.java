9bfdc7c66add57b8abe2aee6db18f2ea7c345e25
ketkeearyamane <ketkeearyamane@gmail.com>
2023-07-10 15:41:51 +0200

    add submit form method

--- src/main/java/com/linkedin/collectibles/controllers/UserController.java
+++ src/main/java/com/linkedin/collectibles/controllers/UserController.java
@@ -1,9 +1,13 @@
 package com.linkedin.collectibles.controllers;
 
 import com.linkedin.collectibles.beans.User;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -11,6 +15,12 @@ public class UserController {
     @GetMapping("/newUser")
     public String displayRegistrationForm(Model model){
         model.addAttribute("user", new User());
+        return "register-user";
+    }
+
+    @PostMapping("/saveUser")
+    public String saveUser(@ModelAttribute("user") @Valid User user, BindingResult result, Model model){
+        //validate & save to DB
         return "register-user";
     }
 }
