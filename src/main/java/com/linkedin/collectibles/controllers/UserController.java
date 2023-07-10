d447fc5d87642e4c52fe715e2ed4025d1970fd3b
ketkeearyamane <ketkeearyamane@gmail.com>
2023-07-10 16:28:08 +0200

    register custom validator

--- src/main/java/com/linkedin/collectibles/controllers/UserController.java
+++ src/main/java/com/linkedin/collectibles/controllers/UserController.java
@@ -1,16 +1,30 @@
 package com.linkedin.collectibles.controllers;
 
 import com.linkedin.collectibles.beans.User;
+import com.linkedin.collectibles.validators.UserValidator;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+
+    private UserValidator userValidator;
+
+    public UserController(UserValidator userValidator) {
+        this.userValidator = userValidator;
+    }
+
+    @InitBinder
+    public void bindUser(WebDataBinder binder){
+        binder.addValidators(this.userValidator);
+    }
 
     @GetMapping("/newUser")
     public String displayRegistrationForm(Model model){
