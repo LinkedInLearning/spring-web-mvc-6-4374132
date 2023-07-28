f7e7988b326d3a325338e5d6d25172d5ef84fa29
ketkeearyamane <ketkeearyamane@gmail.com>
2023-07-28 11:18:01 +0200

    add rest controller

---
+++ src/main/java/com/linkedin/collectibles/restcontrollers/ProductRestController.java
@@ -0,0 +1,32 @@
+package com.linkedin.collectibles.restcontrollers;
+
+import com.linkedin.collectibles.beans.Product;
+import com.linkedin.collectibles.dao.ProductRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ProductRestController {
+
+    private ProductRepository productRepository;
+
+    public ProductRestController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping("/bigstar/api/products")
+    public List<Product> allProducts(){
+        List<Product> products = new ArrayList<>();
+        return (List<Product>) productRepository.findAll();
+
+    }
+
+    @GetMapping("/bigstar/api/products/{id}")
+    public Product getProductById(@PathVariable("id") String id){
+        return productRepository.findById(Integer.valueOf(id)).get();
+    }
+}
