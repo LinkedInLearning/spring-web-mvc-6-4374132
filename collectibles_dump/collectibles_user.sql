a69a96e53a7f19ddb9615c88aa7cd6b30b3c19dd
ketkeearyamane <ketkeearyamane@gmail.com>
2023-08-07 14:25:21 +0200

    update DB scripts

--- collectibles_dump/collectibles_user.sql
+++ collectibles_dump/collectibles_user.sql
@@ -32,7 +32,7 @@ CREATE TABLE `user` (
   `name` varchar(255) DEFAULT NULL,
   `news_letter` bit(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=409 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=420 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -53,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-28 10:07:47
+-- Dump completed on 2023-08-06 21:00:41
